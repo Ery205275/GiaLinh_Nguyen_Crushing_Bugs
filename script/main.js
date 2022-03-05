7e045126ce4566fc142fcc9430e5f414d22ad85b
Nguyen Gia Linh <nguyengialinh@Nguyens-MacBook-Pro.local>
2022-03-06 01:58:09 +0700

    changing thumbnails

--- script/main.js
+++ script/main.js
@@ -1,4 +1,17 @@
 (() => {
-	// set up the puzzle pieces and boards
+	// make connections to the elements on the page
+	// that we want the user to interact
+	const theButtons = document.querySelectorAll("#buttonHolder img"),
+	      theGameBoard = document.querySelector(".puzzle-board");
+
+	function changeBgImg(){
+		//debugger;
+		let key = this.dataset.bgref;
+		console.log(key);
+
+		theGameBoard.style.backgroundImage = `url(images/backGround${key}.jpg)`;
+	}
+    theButtons.forEach(button => button.addEventListener("click", changeBgImg));
+
 	
 })();
